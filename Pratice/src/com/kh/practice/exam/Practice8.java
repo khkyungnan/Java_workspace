97ad7f185de9e7c2fe1d0c443d66a76129d42fed
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-08-31 17:47:20 +0900

    java for pratice

---
+++ Pratice/src/com/kh/practice/exam/Practice8.java
@@ -0,0 +1,22 @@
+package com.kh.practice.exam;
+
+import java.util.Scanner;
+
+public class Practice8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요:");
+		int num = sc.nextInt();
+		
+		if(num < 1) { //만일 1 미만의 숫자가 입력이 되거나 입력한 수는 1보다 크거나 같아야 한다. 
+			System.out.println("1 이상의 숫자를 입력해주세요.");
+		} else { //1 이상인 숫자
+			for ( ;  ; ) {//for문 안에 완성하기
+				System.out.println("");
+			}
+		}
+	}
+}
+
---
+++ Pratice/src/com/kh/practice/exam/Practice9.java
@@ -0,0 +1,22 @@
+package com.kh.practice.exam;
+
+import java.util.Scanner;
+
+public class Practice9 {
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+	        
+         System.out.print("숫자를 입력하세요: ");
+         int num = scanner.nextInt();
+         
+         for() {
+        	 //+= 사용하기
+         }
+         System.out.println("1부터 합 출력하기"+ 변수명);
+	}
+}
+
+
+
+
+
