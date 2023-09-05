795a5d412864983c19a93e50bf3de6817b251d19
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-06 08:55:03 +0900

    ff

--- chap08_class/src/com/kh/classSample/Class01.java
+++ chap08_class/src/com/kh/classSample/Class01.java
@@ -1,6 +1,6 @@
 package com.kh.classSample;
 
-public class Class01(String color) { //매개변수 or 파라미터
+public class Class01{ //매개변수 or 파라미터
 	//어떤 색상을 입력하세요;
 	String color;
 	String name; //이 사람의 이름
@@ -11,13 +11,13 @@ public class Class01(String color) { //매개변수 or 파라미터
 
 	}
 /*
-public class Class01 {
-	String name; //이름
-	int grade; //사람의 학년
-	int classNo; //사람의 반 번호
-	int number; //사람의 출석번호
-}
-	
+	public class Class01 (String color)  {
+		String name; //이름
+		int grade; //사람의 학년
+		int classNo; //사람의 반 번호
+		int number; //사람의 출석번호
+	}
+		
 */	
 	
 	//한 반에 학급인원이 30명이 있다.
