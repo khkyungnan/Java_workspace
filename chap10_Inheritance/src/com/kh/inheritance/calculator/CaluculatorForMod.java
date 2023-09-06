42146806e042c1bd15a9d379a914dafd6221e446
khkyungnan <kimkyungnan@iei.or.kr>
2023-09-06 21:44:20 +0900

    ff

--- chap10_Inheritance/src/com/kh/inheritance/calculator/CaluculatorForMod.java
+++ chap10_Inheritance/src/com/kh/inheritance/calculator/CaluculatorForMod.java
@@ -2,7 +2,7 @@ package com.kh.inheritance.calculator;
 
 /*나눈다음 나머지를 구할 수 있는 기능이 추가된 계산기로 업그레이드!*/
 public class CaluculatorForMod extends Calculator {
-	public int mod(int num1, int num2) {
-		return num1 % num2;
+	public int mod(int a, int b) {
+		return a % b;
 	}
 }
--- chap10_Inheritance/src/com/kh/inheritance/calculator/CaluculatorRun.java
+++ chap10_Inheritance/src/com/kh/inheritance/calculator/CaluculatorRun.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class CaluculatorRun extends CaluculatorForMod {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("첫 숫자를 입력하세요.");
+		int num1 = sc.nextInt();
+		System.out.println("두 번째 숫자를 입력하세요.");
+		int num2 = sc.nextInt();
+		CaluculatorForMod cal = new CaluculatorForMod();
+		//cal.num1 = num1;
+		//cal.num2 = num2;
 		//CaluculatorForMod cal = new CaluculatorForMod();
-	//	System.out.println("add = " + cal.add());
-		//System.out.println("mul =  " + cal.multiply());
-	//	System.out.println("mod = " + cal.mod(num1,num2));
+		System.out.println("add = " + cal.add());
+		System.out.println("mul =  " + cal.multiply());
+		System.out.println("mod = " + cal.mod(num1,num2));
 		
 		/* scanner 이용해서 코드 변경하기 */
-
+/*
 		CaluculatorForMod cal = new CaluculatorForMod();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("첫 숫자를 입력하세요.");
@@ -28,7 +36,7 @@ public class CaluculatorRun extends CaluculatorForMod {
 		System.out.println("mul : " + result3);
 		System.out.println("div : " + result4);
 		System.out.println("div : " + result5);
-		
+		*/
 		
 	}
 
