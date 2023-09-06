577cc3faf0809e2e43a9fa305402ba5b41af6631
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-06 11:05:56 +0900

    GSValid

--- chap09_Getter_Setter/src/com/kh/gettersetter/goodEaxm/GameRun.java
+++ chap09_Getter_Setter/src/com/kh/gettersetter/goodEaxm/GameRun.java
@@ -1,20 +1,26 @@
-package com.kh.gettersetter;
+package com.kh.gettersetter.goodEaxm;
 
 public class GameRun {
 
 	public static void main(String[] args) {
 		// 플레이 생성자 만들기
-		PlayerCharacter player1 = new PlayerCharacter();
+		GoodPlayer player1 = new GoodPlayer();
 		
 		
 		player1.setName("KH");
 		//문제점 : 0-10 = -10
+		
+		
 		player1.setHealth(-10);
+		player1.isValidHealth();
+		
+		
+		
 		player1.setAttackPower(-20);
 		
 		player1.displayInfo();
-		
-		PlayerCharacter player2 = new PlayerCharacter();
+		/*
+		GoodPlayer player2 = new GoodPlayer();
 		player2.setName("ㅎㅎㅎ");
 		player2.setHealth(80);
 		player2.setAttackPower(10);
@@ -23,7 +29,7 @@ public class GameRun {
 		
 		System.out.println("attack!!!! 후");
 		
-		
+		*/
 	}
 
 }
---
+++ chap09_Getter_Setter/src/com/kh/gettersetter/goodEaxm/XXXXX.java
@@ -0,0 +1,5 @@
+package com.kh.gettersetter.goodEaxm;
+
+public class XXXXX {
+
+}
