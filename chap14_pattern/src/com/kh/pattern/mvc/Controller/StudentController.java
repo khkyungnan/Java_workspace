1c7f9d073fcf74839cd406c11c9704327a05e937
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-08 14:19:07 +0900

    Ff

--- chap14_pattern/src/com/kh/pattern/mvc/Controller/StudentController.java
+++ chap14_pattern/src/com/kh/pattern/mvc/Controller/StudentController.java
@@ -1,7 +1,7 @@
 package com.kh.pattern.mvc.Controller;
 
 import com.kh.pattern.mvc.Model.StudentModel;
-import com.kh.pattern.mvc.View.UsrView;
+import com.kh.pattern.mvc.View.StudentView;
 
 /*
  * 모델과 뷰를 연결하는 곳
@@ -11,10 +11,41 @@ public class StudentController {
 	//StudentModel
 	//StudentView
 	private StudentModel studentmodel;
-
-	//private StudentView userview;
+	private StudentView studentview;
 	
-	public StudentController(StudentModel studentmodel) {
+	public StudentController(StudentModel studentmodel,StudentView studentview) {
 		this.studentmodel = studentmodel;
+		this.studentview = studentview;
+		
 	}
+	
+	public void setStudentName(String name) {
+		studentmodel.setName(name);
+	}
+	
+	public void setStudentAge(int Age) {
+		studentmodel.setAge(Age);
+		
+	}
+	
+	public String getStudentName() {
+		return studentmodel.getName();
+	}
+	
+	public int getStudentAge() {
+		return studentmodel.getAge();
+	}
+	
+	public void updateView() {
+		//DB에 변경될 값 추가
+		studentview.displayInfor(studentmodel.getName(), studentmodel.getAge());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
