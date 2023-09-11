4bf38a2a381cdfac93b8ead872add6b5003ece3c
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-11 17:40:19 +0900

    ff

--- chap17_List/src/com/kh/ListAndArray/ListAndArray.java
+++ chap17_List/src/com/kh/ListAndArray/ListAndArray.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ListAndArray {
 
 	public static void main(String[] args) {
-		ListArray1();
+		ListArray1(); //static
 		ListAndArray LAA = new ListAndArray();
 		LAA.ListArray2();
 	}
