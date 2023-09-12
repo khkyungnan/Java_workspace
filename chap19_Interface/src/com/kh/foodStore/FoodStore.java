4a4873dc3e744227b04f87cd22bd9fe318199ddf
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-12 12:35:27 +0900

    FF

---
+++ chap19_Interface/src/com/kh/foodStore/FoodStore.java
@@ -0,0 +1,6 @@
+package com.kh.foodStore;
+
+public interface FoodStore {
+	void addMenu(String menu);
+	void removeMenu(String menu);
+}
---
+++ chap19_Interface/src/com/kh/foodStore/Main.java
@@ -0,0 +1,12 @@
+package com.kh.foodStore;
+
+public class Main {
+	public static void main(String[] args) {
+	//            FoodStore-> Order
+		FoodStore order = new Order();
+		order.addMenu("Â«»ÍÁÖ¹®");
+		order.addMenu("Â¥Àå¸éÁÖ¹®");
+		order.addMenu("±¼Â«»ÍÁÖ¹®");
+	}
+
+}
