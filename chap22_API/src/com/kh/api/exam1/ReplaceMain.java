10a59e32b0ac2da2a6a1fd071a030af969343a25
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-13 14:19:31 +0900

    FFF

--- chap22_API/src/com/kh/api/exam1/ReplaceMain.java
+++ chap22_API/src/com/kh/api/exam1/ReplaceMain.java
@@ -3,8 +3,20 @@ package com.kh.api.exam1;
 public class ReplaceMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//replace1();
+	}
+	
+	public void replace1() {
+		String input = "Hello, world!";
+		String reInput = input.replace(" ", "-");
+		System.out.println(reInput);
+		String input2 = reInput.replace("-", "");
+		System.out.println("input2 : "+input2);
+	}
+	
+	public void replace2() {
+		String input = "Hello World!";
+		//World를 Java로 교체
 	}
 
 }
