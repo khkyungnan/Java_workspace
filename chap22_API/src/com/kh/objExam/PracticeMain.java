933c2cc2d38e7d51955d4f53554d199245649acb
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-13 11:30:28 +0900

    FFF

---
+++ chap22_API/src/com/kh/objExam/PracticeMain.java
@@ -0,0 +1,21 @@
+package com.kh.objExam;
+
+public class PracticeMain {
+/*
+com.kh.objExam
+	PracticeMain
+	public void practice1
+		Scanner 이용해서 
+			equals if
+			hashcode
+			toString
+ * */
+	public static void main(String[] args) {
+		// practice1();
+
+	}
+	public void practice1() {
+		//Scanner 이용해서 구현
+	}
+
+}
