1323b0e857f6c9c84815ecea19aad140bea0e44c
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-13 14:29:04 +0900

    FF

--- chap22_API/src/com/kh/api/exam1/ReplaceMain.java
+++ chap22_API/src/com/kh/api/exam1/ReplaceMain.java
@@ -4,6 +4,8 @@ public class ReplaceMain {
 
 	public static void main(String[] args) {
 		//replace1();
+		ReplaceMain rm = new ReplaceMain();
+		rm.replace2();
 	}
 	
 	public void replace1() {
@@ -17,6 +19,26 @@ public class ReplaceMain {
 	public void replace2() {
 		String input = "Hello World!";
 		//World를 Java로 교체
+		String reInput = input.replace("World", "Java");
+		System.out.println(reInput);
 	}
-
+	
+	public void replace3() {
+		String originName = "Hello world";
+		// o로 적혀있는 문자열들 x로 변경하기
+	}
+	public void replace4() {
+		String originString = "The quick brown fox jumps over the lazy dog";
+		// fox -> cat
+	}
+	public void replace5() {
+		String originString = "The quick brown fox jumps over the lazy dog";
+		// 모든 공백 제거하기
+	}
+	
+	public void replaceAll() {
+		String originString = "Hello World";
+		// replace 대신 replaceAll 을 이용해서 대소문자 구분없이 문자열 치환하기
+		String reAllstr = originString.replaceAll("(?i)world", )
+	}
 }
