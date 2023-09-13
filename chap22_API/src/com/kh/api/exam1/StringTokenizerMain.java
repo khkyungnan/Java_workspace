a8e1e886a3eb1cf69d4b61a4c2e00f8c79f93bd1
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-13 11:51:27 +0900

    FF

--- chap22_API/src/com/kh/api/exam1/StringTokenizerMain.java
+++ chap22_API/src/com/kh/api/exam1/StringTokenizerMain.java
@@ -8,6 +8,11 @@ public class StringTokenizerMain {
  						주어진 구분자(기본:공백)를 기분으로 문자열을 토큰으로 분리
  * */
 	public static void main(String[] args) {
+		StringTokenizerMain stm = new StringTokenizerMain();
+		//stm.STExam();
+		stm.stExam2();
+	}
+	public void STExam() {
 		String text="apple orange banana";
 		StringTokenizer tokenizer = new StringTokenizer(text);
 		//tokenizer.
@@ -16,8 +21,20 @@ public class StringTokenizerMain {
 			String token = tokenizer.nextToken();
 			System.out.println(token);
 		}
-		
-
 	}
 
+	public void stExam2() {
+		String text = "apple,banana,grape,orange";
+		StringTokenizer tokenizer = new StringTokenizer(text, ",");
+		
+		while(tokenizer.hasMoreTokens()) {
+			String fruit = tokenizer.nextToken();
+			System.out.println(fruit);
+		}
+	}
 }
+
+
+
+
+
