6ee50c2aaddb6b703a70745eab1469be433acafc
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-13 14:52:52 +0900

    FF

--- chap22_API/src/com/kh/api/exam1/ReplaceMain.java
+++ chap22_API/src/com/kh/api/exam1/ReplaceMain.java
@@ -6,6 +6,10 @@ public class ReplaceMain {
 		//replace1();
 		ReplaceMain rm = new ReplaceMain();
 		rm.replace2();
+		rm.replace3();
+		rm.replace4();
+		rm.replace5();
+		rm.replaceAll();
 	}
 	
 	public void replace1() {
@@ -26,19 +30,29 @@ public class ReplaceMain {
 	public void replace3() {
 		String originName = "Hello world";
 		// o로 적혀있는 문자열들 x로 변경하기
+		String reString = originName.replace("o", "x");
+		System.out.println(reString);
 	}
 	public void replace4() {
 		String originString = "The quick brown fox jumps over the lazy dog";
 		// fox -> cat
+		String reString = originString.replace("fox", "cat");
+		System.out.println(reString);
+		
 	}
 	public void replace5() {
 		String originString = "The quick brown fox jumps over the lazy dog";
 		// 모든 공백 제거하기
+		String reStr = originString.replace(" ", "");
+		System.out.println(reStr);
+		String reAllStr = originString.replaceAll("\\s", "");
+		System.out.println(reAllStr);
 	}
 	
 	public void replaceAll() {
 		String originString = "Hello World";
-		// replace 대신 replaceAll 을 이용해서 대소문자 구분없이 문자열 치환하기
-		String reAllstr = originString.replaceAll("(?i)world", )
+		// replace 대신 replaceAll 을 이용해서 대소문자 구분없이 문자열 Java로 치환하기
+		String reAllstr = originString.replaceAll("(?i)world", "");
+		System.out.println(reAllstr);
 	}
 }
