937ca5dc6133eae6a4f12439a9eb669ab790f3cc
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-14 16:29:10 +0900

    FF

--- chap24_입출력 (Input0utput)/src/com/kh/practice/Practice_mkdirs.java
+++ chap24_입출력 (Input0utput)/src/com/kh/practice/Practice_mkdirs.java
@@ -10,6 +10,6 @@ public class Practice_mkdirs {
 	public void Practice1() {
 		//폴더 연속으로 2개 만들기 C:\\Users\\user1\\Desktop\\이름1\\이름2
 		//폴더 연속으로 3개 만들기 C:\\Users\\user1\\Java_Workspace\\chap25_
-		//폴더 연속으로 5개 만들기 C:\\Users\\user1\\Documents\\myPlace\\Music\\Korean\\kPop\\Group
+		//폴더 연속으로 5개 만들기 C:\\Users\\user1\\Desktop\\myPlace\\Music\\Korean\\kPop\\Group
 	}
 }
