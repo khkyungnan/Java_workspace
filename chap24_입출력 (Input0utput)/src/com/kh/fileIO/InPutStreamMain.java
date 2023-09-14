4df9655d3651a23f123e428cc628c2359c931d79
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-14 15:01:32 +0900

    FF

---
+++ chap24_입출력 (Input0utput)/src/com/kh/fileIO/InPutStreamMain.java
@@ -0,0 +1,23 @@
+package com.kh.fileIO;
+
+public class InPutStreamMain {
+/*
+ FileInputStream 
+ 	파일을 읽을 때 사용되는 클래스
+ 	파일로부터 데이터를 바이트 스트림으로 읽어들일 때 유용하게 사용
+ 	int read() : 파일에서 다음  데이터를 읽고 반환
+ 				더 이상 읽을 데이터가 없으면 -1을 반환
+ 	int read(byte[] b): 파일에서 파이트 배열 'b'에 데이터를 읽고 
+ 						실제로 읽은 바이트 수를 반환
+ 	int read(byte[] b, int off, int len):파일에서 바이트 배열 'b'의 
+ 						오프셋의 위치부터 최대 len 바이트 만큼 읽어서
+ 						실제로 읽은 바이트 수를 반환
+ 		void close() 파일 닫기
+ 	
+ * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
