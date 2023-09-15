50f2b5f3d8c0797f6930b077e4afc4f506c159a1
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-15 09:30:18 +0900

    FF

--- chap24_입출력 (Input0utput)/src/com/kh/fileIO/FileCopyMain.java
+++ chap24_입출력 (Input0utput)/src/com/kh/fileIO/FileCopyMain.java
@@ -10,9 +10,18 @@ public class FileCopyMain {
 	public static void main(String[] args) {
 		String inputFile ="C:\\Users\\user1\\Desktop\\cute.png";
 		String outputFile = "C:\\Users\\user1\\Desktop\\RealCute.jpg";
-		
-		try (FileInputStream fis = new FileInputStream(inputFile);
-				FileOutputStream fos = new FileOutputStream(outputFile)){
+		/*
+		 try(resource) 와 try{} 차이점
+		 try(resource) 문법은 자동 자원 관리 문법으로 코드가 블록을 빠져 나갈 때 자원을 자동으로 닫을 수 있음
+		 				현재는 File~~~Stream 들을 자동으로 닫겠다 선언한 것
+		 try{...}      문법은 자원을 수동으로 닫아 예외처리를 진행해야함
+		 
+		 try(FileInputStream fis = new FileInputStream(inputFile);
+			FileOutputStream fos = new FileOutputStream(outputFile))
+		 */
+		try{
+			FileInputStream fis = new FileInputStream(inputFile);
+			FileOutputStream fos = new FileOutputStream(outputFile);
 			byte[] buffer = new byte[1024]; //1024 바이트 크기의 버퍼
 			int byteRead; 
 			//int byteRead: fis.read(buffer) 메서드를 호출하여 버퍼로 읽어들인 바이트 수를 저장하는 변수 선언
--- chap24_입출력 (Input0utput)/src/com/kh/practice/PracticeCopy.java
+++ chap24_입출력 (Input0utput)/src/com/kh/practice/PracticeCopy.java
@@ -1,14 +1,24 @@
 package com.kh.practice;
 
+import java.io.File;
+
 public class PracticeCopy {
 
 	public static void main(String[] args) {
 		
 	}
 	public void practice1() {
-		String inputFile ="C:\\Users\\user1\\Desktop\\love.png";
-		String outputFile = "C:\\Users\\user1\\Desktop\\RealLove.jpg";
+		String filePath = "C:\\Users\\user1\\Desktop\\";
+		String inputFile =filePath+"love.png";
+		String outputFile = filePath+"RealLove.jpg";
 		
+		File currentFile = new File(filePath + inputFile);
+		File newFile = new File(filePath + outputFile);
+		if(currentFile.renameTo(newFile)) {
+			System.out.println("파일 이름 변경 성공");
+		} else {
+			System.out.println("파일 이름 변경 실패");
+		}
 	}
 	public void practice2() {
 		//이미지파일 jpg 또는 png 파일을 바탕화면에 저장한 후 
@@ -16,7 +26,7 @@ public class PracticeCopy {
 		//폴더 안에 복사한 이미지 저장하기
 		String inputFile ="C:\\Users\\user1\\Desktop\\cute.png";
 		String newFolder = "C:\\Users\\user1\\Desktop\\img";
-		newFolder.mkdir();
+		if(!newFolder) {
 		String outputFile =newFolder+"\\"+"cute1.jpg";
 	}
 }
