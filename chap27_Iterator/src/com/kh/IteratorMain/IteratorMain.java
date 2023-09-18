fadc72f16564c352f45365f5eb2acc02e028f7e9
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-18 09:38:40 +0900

    FF

---
+++ chap27_Iterator/src/com/kh/IteratorMain/IteratorMain.java
@@ -0,0 +1,34 @@
+package com.kh.IteratorMain;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/*
+Iterator 
+	컬렉션(데이터 크룹)을 반복(iterate)하면서 요소에 접근하는데 사용되는 인터페이스
+	다양한 컬렉션 유형(ArrayList, HashSet 등)에 사용할 수 있음
+	컬렉션의 요소를 읽고 삭제하는데 사용
+	컬렉션의 크기나 내부 구조에 관계없이 요소에 접근할 수 있음
+ * */
+public class IteratorMain {
+
+	public static void main(String[] args) {
+		//AarayLsit 생성 및 요소 추가
+		ArrayList<String> music = new ArrayList<>();
+		music.add("발라드");
+		music.add("댄스");
+		music.add("트로트");
+		
+		// Iterator 생성
+		Iterator<String> iter = music.iterator();
+		
+		//Iterator를 사용하여 요소 반복
+		while(iter.hasNext()) {
+			String msc = iter.next();
+			System.out.println(msc);
+		}
+	}
+
+}
+
+
