70ec17e0e5f87bedbbd92de9f5f7a636c1c5a4f7
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-19 15:12:23 +0900

    FF

--- chap30_Thread/src/com/kh/sync/syncMethod.java
+++ chap30_Thread/src/com/kh/sync/syncMethod.java
@@ -22,17 +22,89 @@ synchronized
 		}
  * */
 
-
-
-
-
-
-
 public class syncMethod {
-
+	private int count = 0;
+	private Object lockObj = new Object(); //동기화를 위한 객체
+	
+	//메서드 레벨 동기화를 사용해서 count 변수를 증가시킴
+	//한 번에 하나의 스레드만 접근할 수 있도록 보장
+	public synchronized void increment() {
+		count++;
+	}
+	
+	/*
+	 pTask : 동기화된 블록과 함께 사용
+	 synchronized(lockObj) " 코드 블록 내에서 count 변수를 증가시키기 전에
+	 						 lockObj를 사용하여 동기화를 수행
+	 lockObj 사용하는 이유 : 다른 메서드에서 동기화에 사용할 수 있고, 
+	 		    다른 객체에 대한 동기화와 분리되어 충돌을 방지할 수 있음
+	 * */
+	public void pTask() {
+		// 다른 비동기 코드
+		
+		synchronized (lockObj) {
+			//lockObj 를 사용한 동기화된 블록
+			count++;
+		}
+		// 다른 비동기 코드
+	}
+	// syncMethod 클래스의 인스턴스를 생성
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		syncMethod sync = new syncMethod();
+		
+		// Runnable 인터페이스 구현을 사용해서 두 개의 스레드 생성
+		//thread1
+		Runnable task1 = new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 1000; i++) {
+					sync.increment();
+				}
+			}
+		};
+		//thread2
+		Runnable task2 = new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 1000; i++) {
+					sync.increment();
+				}
+			}
+		};
+		
+		//스레드 생성 및 시작
+		Thread thread1 = new Thread(task1);
+		Thread thread2 = new Thread(task2);
+		
+		thread1.start();
+		thread2.start();
+		
+		//모든 스레드가 종료될 때 까지 대기
+		try {
+			thread1.join();
+			thread2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("최종카운트 : " + sync.count);
 	}
 
 }
+
+/*
+ * synchronized count 변수에 대한 동시 접근을 동기화
+
+	스레드 간 경쟁 조건이 발생하지 않고
+	count 변수가 안전하게 증가
+	데이터의 일관성을 보존
+ */
+	
+	
+	
+	
+	
+	
+	
+	
+
---
+++ chap33_Graph/src/com/kh/GraphMain/Graph.java
@@ -0,0 +1,25 @@
+package com.kh.GraphMain;
+
+import java.util.LinkedList;
+
+public class Graph {
+	private int V; //그래프의 노드 수
+	private LinkedList<Integer>[] adjList; //인접 리스트 배열
+	
+	//그래프 생성자
+	public Graph(int ver) {
+		V = ver;
+		adjList = new LinkedList[ver];
+		for(int i = 0; i < ver; i++) {
+			adjList[i] = new LinkedList<>();
+		}
+	}
+	
+	//노드를 그래프에 추가
+	public void addEdge(int source, int des)
+}
+
+
+
+
+
