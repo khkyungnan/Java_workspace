30dee1cf81ac20029a9615b3a1d7524d07197d16
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-19 17:46:40 +0900

    FF

--- chap33_Graph/src/com/kh/GraphMain/GraphMain.java
+++ chap33_Graph/src/com/kh/GraphMain/GraphMain.java
@@ -3,7 +3,7 @@ package com.kh.GraphMain;
 public class GraphMain {
 
 	public static void main(String[] args) {
-		int ver = 5; //그래프 노의 수를 5로 정의
+		int ver = 15; //그래프 노의 수를 5로 정의
 		Graph graph = new Graph(ver);
 		
 		//addEdge  간선 추가하겠다.
@@ -11,7 +11,10 @@ public class GraphMain {
 		graph.addEdge(0, 2); //노드 0에서 노드 2로 방향이 있는 엣지를 추가
 		graph.addEdge(1, 3);
 		graph.addEdge(2, 4);
-		
+		graph.addEdge(3, 1); //노드 0에서 노드 1로 방향이 있는 엣지를 추가
+		graph.addEdge(4, 2); //노드 0에서 노드 2로 방향이 있는 엣지를 추가
+		graph.addEdge(5, 3);
+		graph.addEdge(6, 7);
 		System.out.println("DFS 탐색 결과 : ");
 
 		// DFS 탐색을 시작할 노드의 번호를 전달
