c5a01d73e9ef19a052fb6d4fe0374b7cb5215cd8
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-09-21 17:19:45 +0900

    FF

--- chap36_final_practice/src/com/kh/finalSample/TcpServer.java
+++ chap36_final_practice/src/com/kh/finalSample/TcpServer.java
@@ -1,24 +1,22 @@
 package com.kh.finalSample;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpServer {
-	int port =3333;
-	
-	ServerSocket server;
-	
-	server = new ServerSocket(port);
-	sysout(port);
-	while(true) {
-		Socket client = server.accept();
-		sysout (client.getInetAddress());
-		
-		BufferedReader
-		OutputStream
-		
-		client.close();
+	public static void main(String[] args) throws Exception {
+		int port =3333;
+		ServerSocket server;
+		server = new ServerSocket(port);
+		System.out.println("서버가 포트" + port + "에서 실행 중입니다.");
+		while(true) {
+			Socket client = server.accept();
+			System.out.println("클라이언트 접속:" + client.getInetAddress());
+			client.close();
+		}
 	}
+	
 }
 
 
